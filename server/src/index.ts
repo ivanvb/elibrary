646c56e192e4525195ea7f44fff0e206b6cb3da9
ivb98 <ivanvalette@hotmail.com>
2019-11-26 13:50:13 -0400

    Initialized database connected and express server inside 'main' function

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,22 @@
-console.log("hello world")
+import mongoose from 'mongoose';
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+let app: express.Application = express();
+(async function main(){
+    dotenv.config();
+    
+    mongoose.connect(process.env.DATABASE_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true
+    });
+
+    app.use(cors());
+    app.use(express.json());
+    app.use(express.urlencoded({extended: false}));
+
+    const port: number = Number(process.env.PORT) || 3000;
+    app.listen(port);
+})()
